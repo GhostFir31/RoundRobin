02fdf14699e6871d07546eafe7734fdee3dec91a
GhostFir31 <j3sus0marle@gmail.com>
2024-02-07 15:59:50 -0800

    Initial commit

---
+++ RoundRobin.js
@@ -0,0 +1,20 @@
+console.log("Cuantos Procesos se Ejecutaran? ")
+let numProcesos = process.argv[2] ? parseInt(process.argv[2]) : 1;
+console.log("El numero de procesos son: " + numProcesos)
+
+
+function crearProcesoNuevo(){
+    let proceso
+
+    proceso={
+
+        
+
+    }
+
+
+}
+
+
+
+
