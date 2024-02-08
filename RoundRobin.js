11aea77e1330690edc879e772f1d04f15d92cfaa
GhostFir31 <j3sus0marle@gmail.com>
2024-02-07 21:14:42 -0800

    avance

--- RoundRobin.js
+++ RoundRobin.js
@@ -1,5 +1,5 @@
 let id = 1;
-let caracter = 97;
+let caracter = 'a';
 let num1 = 1;
 let num2 = 2;
 let lista = [];
@@ -10,37 +10,42 @@ console.log("Cuantos Procesos se Ejecutaran? ");
 let numProcesos = process.argv[2] ? parseInt(process.argv[2]) : 1;
 console.log("El numero de procesos son: " + numProcesos);
 
-do {
+
   for (i = 0; i < numProcesos; i++) {
+
     lista[i] = crearProcesoNuevo();
+    
   }
 
   switch (contador) {
-    case 0:
-      break;
-    case 1:
-      break;
-    case 2:
-      break;
+
+    case 0: console.log(lista[contador].proceso.numeroProceso); break;
+
+    case 1: console.log(lista[contador].proceso.cadena1); break;
+
+    case 2:console.log( lista[contador].proceso.cadena2); break;
+
+    case 3: console.log(lista[contador].proceso.cadena3); break
+
   }
 
-  contador++;
-} while (contador < numProcesos);
 
 function crearProcesoNuevo() {
   let proceso;
 
   if (id > 1) {
-    caracter = caracter + id;
+    caracter = String.fromCharCode(caracter.charCodeAt(0) + 1);;
   }
 
   proceso = {
-    numeroProceso: id,
 
+    numeroProceso: id,
     cadena1: "" + caracter + "=" + num1 + "+" + num2,
     cadena2: "console.log(" + num1 + "+" + num2 + ")",
     cadena3: "console.log(" + caracter + ")",
+
   }
 
+  id++;
   return proceso;
 }
